b4246ab2ce1f1a0feb463075c268e33a06c3e3cb
Rhubert710 <73670513+Rhubert710@users.noreply.github.com>
2022-06-10 02:17:30 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from "react";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ToDoComponent />
       </header>
     </div>
   );
 }
 
+const ToDoComponent = () =>
+{
+  let [title, setTitle] = useState('');
+  let [description, setDescription] = useState('');
+
+  return(
+<div id='todo_div'>
+
+      {/* TITLE INPUT */}
+      <div id='title_input_label' >Title:</div>
+      <input id='title_input' type="text" style={{width:'19vw'}} 
+        value={title} onChange = {(e)=>
+        {
+          let new_title = e.target.value;
+          setTitle(new_title);
+        }}
+      />
+
+      {/* DESCRIPTION INPUT */}
+      <div id='description_input_label' >Description:</div>
+      <textarea id='description_input' type="text" style={{width:'26vw', height:'95px'}} value={description} onChange = {(e)=>
+        {
+          let new_description = e.target.value;
+          setDescription(new_description);
+        }}
+      />
+</div>
+  )
+}
 export default App;
+
